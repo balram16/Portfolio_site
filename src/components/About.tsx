3defe8c1fd9a9772858c933f0c7665ddc0efec84
Balram Panigrahi <141282145+balram16@users.noreply.github.com>
2025-08-13 00:50:06 +0530

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -213,7 +213,7 @@ const About: React.FC<AboutProps> = ({ theme }) => {
             transition={{ type: 'spring', stiffness: 300 }}
           >
             <Image 
-              src="https://drive.google.com/file/d/1qxBRHfyAGkK7fR6QFBnZFfIL1mmOaPyz/view?usp=drive_link" 
+              src="https://i.postimg.cc/bJVNkb3S/me.jpg" 
               alt="Balram Panigrahi" 
               fill 
               style={{ objectFit: 'cover' }}
